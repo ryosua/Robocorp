3e70767be6906daeeeaffd02bc8f62ed2409720e
Ryan Yosua <rcy5017@psu.edu>
2015-03-11 20:26:51 -0400

    Register user form prints inputs.

---
+++ Assets/Scripts/UserController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+// Other code.
+
+public class UserController : MonoBehaviour {
+
+	public InputField emailField;
+	public InputField passwordField;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void RegisterUser () {
+		string email = emailField.text;
+		string password = passwordField.text;
+
+		print("The form was submitted.");
+		print("Email: " + email);
+		print("Password: " + password);
+	}
+	
+}
