426b711b3050124f7626c8cbb0d533fe576ce82c
Ryan Yosua <rcy5017@psu.edu>
2015-03-22 20:44:03 -0400

    Started in-game UI.

---
+++ Assets/Scripts/GameUIController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameUIController : MonoBehaviour {
+
+	public Text playerText;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnEndTurnPress () {
+		if (playerText.text == "Player 1") {
+			playerText.text = "Player 2";
+		}
+		else if (playerText.text == "Player 2") {
+			playerText.text = "Player 1";
+		}
+	}
+
+}
