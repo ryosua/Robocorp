9ec2bde48a28b6048735005a72ecaea5ec182aee
sean-lane <sjl5306@psu.edu>
2015-03-26 18:12:24 -0400

    Movement system completed
    Map is stitched together, units now move with arrow keys

--- Assets/Scripts/GroundScript.cs
+++ Assets/Scripts/GroundScript.cs
@@ -3,17 +3,18 @@ using System.Collections;
 
 public class GroundScript : MonoBehaviour {
 
-	GroundScript up_block;
-	GroundScript down_block;
-	GroundScript left_block;
-	GroundScript right_block;
+	public GameObject up_block;
+	public GameObject down_block;
+	public GameObject left_block;
+	public GameObject right_block;
+	public bool occupied;
 
 	// public string for the tile type
 	public string tileType;
 
 	// Use this for initialization
 	void Start () {
-	
+		occupied = false;
 	}
 	
 	// Update is called once per frame
