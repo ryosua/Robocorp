676d2bd9c0dbd7ade2f181e235060c1117d99629
Ryan Yosua <rcy5017@psu.edu>
2015-04-06 21:12:35 -0400

    Started build menu.

---
+++ Assets/Scripts/CloseBuildMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloseBuildMenu : MonoBehaviour {
+
+	public GameObject buildMenu;
+
+	public void OnClick () {
+		buildMenu.GetComponent<CanvasGroup>().alpha = 0;
+		print("Closing Build menu.");
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/OpenBuildMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenBuildMenu : MonoBehaviour {
+
+	public GameObject buildMenu;
+	public GameObject baseMenu;
+
+	public void OnClick () {
+		buildMenu.GetComponent<CanvasGroup>().alpha = 1;
+		buildMenu.SetActive (true);
+		print("Opening Build menu.");
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
