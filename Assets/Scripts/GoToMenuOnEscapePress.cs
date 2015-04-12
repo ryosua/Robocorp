ae0a64791a0e99b86c72c28f007b15e9de1cf39a
Ryan Yosua <rcy5017@psu.edu>
2015-04-11 21:48:00 -0400

    Go to the main menu if the escape button is pressed.

---
+++ Assets/Scripts/GoToMenuOnEscapePress.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToMenuOnEscapePress : MonoBehaviour {
+
+	public SceneController sceneController;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Escape)) {
+			sceneController.GoToMenuScene ();
+		}
+	}
+}
