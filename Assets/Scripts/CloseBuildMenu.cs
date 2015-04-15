aa76546c94b9e539f55ec726d5f71d460ed6e255
sean-lane <sjl5306@psu.edu>
2015-04-15 14:26:54 -0400

    Updated UI
    Updated UI

--- Assets/Scripts/CloseBuildMenu.cs
+++ Assets/Scripts/CloseBuildMenu.cs
@@ -4,16 +4,16 @@ using System.Collections;
 public class CloseBuildMenu : MonoBehaviour {
 
 	public GameObject MainCamera;
+	public BuildController cntl;
 	
-	public void OnClick () {
-		BuildController cntl = MainCamera.GetComponent<BuildController> ();
+	public void OnClick () { 
 		cntl.SetBuildMenuVisible (false);
 		cntl.CloseUnitPanel (cntl.getSelectedPawnType ());
 	}
 
 	// Use this for initialization
 	void Start () {
-	
+		cntl = MainCamera.GetComponent<BuildController> ();
 	}
 	
 	// Update is called once per frame
